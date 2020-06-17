ef226eab15b1294e5d7bc20e805148aa864bc833
Klem <kaelhem@gmail.com>
2020-06-18 00:14:08 +0200

    fix overflow in footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FOOTER_HEIGHT } from '../constants'
 
 const Footer = ({version}) => (
-  <div className="app-footer" style={{ height: FOOTER_HEIGHT }}>
+  <div className="app-footer" style={{ height: FOOTER_HEIGHT, overflowY: 'hidden' }}>
     <div>Moiki Exporter - v{version} {'//'} kaelhem ©2020</div>
   </div>
 )
